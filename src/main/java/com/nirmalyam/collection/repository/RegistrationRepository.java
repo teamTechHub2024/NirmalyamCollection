6363b665449df70715fd605cb221287e7c85909d
teamTechHub2024 <snoopydurbaka@gmail.com>
2025-01-28 08:46:08 +0530

    added repository annotation

--- src/main/java/com/nirmalyam/collection/repository/RegistrationRepository.java
+++ src/main/java/com/nirmalyam/collection/repository/RegistrationRepository.java
@@ -3,5 +3,6 @@ package com.nirmalyam.collection.repository;
 import com.nirmalyam.collection.model.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@repository
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
 }
